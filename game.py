f1fbe21b94c573d18c2045d6d4377409f4b412f9
John <dimapodschibiakin@yandex.ru>
2024-11-14 11:59:42 +0700

    9 lesson

--- game.py
+++ game.py
@@ -4,63 +4,84 @@ from gameparts import TicTacToe
 
 from gameparts import TicTacToe
 from gameparts.exceptions import InvalidFieldError, NotEmptyFieldError
+import pygame_test
 
 def main():
     tic = TicTacToe()
     current_player = 'X'
     running = True
+    pygame_test.pygame.init()
+    pygame_test.draw_lines()
 
     while running:
-        print(f'У {current_player}-ка есть шанс изменить ситуацию на поле')
+        for event in pygame_test.pygame.event.get():
+            if event.type == pygame_test.pygame.QUIT:
+                running = False
 
-        while True:
-            try:
-                row = int(input('Введите номер строки: '))
-                column = int(input('Введите номер столбца: '))
+            if event.type == pygame_test.pygame.MOUSEBUTTONDOWN:
+                mouse_y = event.pos[0]
+                mouse_x = event.pos[1]
 
-                if row < 0 or row >= tic._field_size:
-                    raise InvalidFieldError
-                if column < 0 or column >= tic._field_size:
-                    raise InvalidFieldError
-                if tic.board[row][column] != ' ':
-                    raise NotEmptyFieldError
-            except NotEmptyFieldError as E:
-                print(E)
-                print('Введите другую ячейку')
-                continue
-            except InvalidFieldError:
-                print(f'Выбирете значения удовл требованию:'  
-                      f' 0 <= значение <= {tic._field_size}')
-                continue
-            except ValueError:
-                print('Буквы вводить нельзя. Только числа.')
-                print('Пожалуйста, введите значения для строки и столбца заново.')
-                continue
-            except Exception as E:
-                print(f'Возникла ошибка {E}')
-            else:
-                break
+                clicked_row = mouse_x // pygame_test.CELL_SIZE
+                clicked_col = mouse_y // pygame_test.CELL_SIZE
 
-        tic.make_move(row, column, current_player)
-        tic.display()
+                if tic.board[clicked_row][clicked_col] == ' ':
+                    tic.make_move(clicked_row, clicked_col, current_player)
+                    current_player = 'O' if current_player == 'X' else 'X'
+
+            if tic.is_board_full():
+                rs = 'Ничья\n'
+                print(rs)
+                save_result(rs)
+                running = False
+
+            if tic.check_win():
+                rs = f'Победу одержал {current_player}-ик.'
+                print(rs)
+                save_result(f'Победили {current_player}\n')
+                running = False
+
+            pygame_test.draw_figures(tic.board)
+        pygame_test.pygame.display.update()
+
+    pygame_test.pygame.quit()
+
+        #print(f'У {current_player}-ка есть шанс изменить ситуацию на поле')
+
+        #while True:
+            #try:
+                #row = int(input('Введите номер строки: '))
+                #column = int(input('Введите номер столбца: '))
+
+                #if row < 0 or row >= tic._field_size:
+                    #raise InvalidFieldError
+                #if column < 0 or column >= tic._field_size:
+                    #raise InvalidFieldError
+                #if tic.board[row][column] != ' ':
+                    #raise NotEmptyFieldError
+            #except NotEmptyFieldError as E:
+                #print(E)
+                #print('Введите другую ячейку')
+                #continue
+            #except InvalidFieldError:
+                #print(f'Выбирете значения удовл требованию:'
+                      #f' 0 <= значение <= {tic._field_size}')
+                #continue
+            #except ValueError:
+                #print('Буквы вводить нельзя. Только числа.')
+                #print('Пожалуйста, введите значения для строки и столбца заново.')
+                #continue
+            #except Exception as E:
+                #print(f'Возникла ошибка {E}')
+            #else:
+                #break
 
 
 
-        if tic.check_win():
-            print(f'Победу одержал {current_player}-ик.')
-            running = False
 
-        if tic.is_board_full():
-            print('Ничья')
-            running = False
-
-        current_player = 'O' if current_player == 'X' else 'X'
-
-
-
-
-
-    print('hello')
+def save_result(result: str):
+    with open ('results.txt', 'a', encoding='utf-8') as file:
+        file.write(result)
 
 if __name__ == '__main__':
     main()
---
+++ pygame_test.py
@@ -0,0 +1,88 @@
+# pygame_test.py
+
+# Импортировать библиотеку Pygame.
+import pygame
+
+# Инициализировать библиотеку Pygame.
+pygame.init()
+
+CELL_SIZE = 100
+BOARD_SIZE = 3
+WIDTH = HEIGHT = CELL_SIZE * BOARD_SIZE
+LINE_WIDTH = 15
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+X_COLOR = (84, 84, 84)
+O_COLOR = (242, 235, 211)
+X_WIDTH = 15
+O_WIDTH = 15
+SPACE = CELL_SIZE // 4
+
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
+pygame.display.set_caption('Крестики-нолики')
+screen.fill(BG_COLOR)
+
+def draw_lines():
+    # Горизонтальные линии.
+    for i in range(1, BOARD_SIZE):
+        pygame.draw.line(
+            screen,
+            LINE_COLOR,
+            (0, i * CELL_SIZE),
+            (WIDTH, i * CELL_SIZE),
+            LINE_WIDTH
+        )
+
+    # Вертикальные линии.
+    for i in range(1, BOARD_SIZE):
+        pygame.draw.line(
+            screen,
+            LINE_COLOR,
+            (i * CELL_SIZE, 0),
+            (i * CELL_SIZE, HEIGHT),
+            LINE_WIDTH
+        )
+
+# Функция, которая отвечает за отрисовку фигур
+# (крестиков и ноликов) на доске.
+def draw_figures(board):
+    for row in range(BOARD_SIZE):
+        for col in range(BOARD_SIZE):
+            if board[row][col] == 'X':
+                pygame.draw.line(
+                    screen,
+                    X_COLOR,
+                    (col * CELL_SIZE + SPACE, row * CELL_SIZE + SPACE),
+                    (
+                        col * CELL_SIZE + CELL_SIZE - SPACE,
+                        row * CELL_SIZE + CELL_SIZE - SPACE
+                    ),
+                    X_WIDTH
+                )
+                pygame.draw.line(
+                    screen,
+                    X_COLOR,
+                    (
+                        col * CELL_SIZE + SPACE,
+                        row * CELL_SIZE + CELL_SIZE - SPACE
+                    ),
+                    (
+                        col * CELL_SIZE + CELL_SIZE - SPACE,
+                        row * CELL_SIZE + SPACE
+                    ),
+                    X_WIDTH
+                )
+            elif board[row][col] == 'O':
+                pygame.draw.circle(
+                    screen,
+                    O_COLOR,
+                    (
+                        col * CELL_SIZE + CELL_SIZE // 2,
+                        row * CELL_SIZE + CELL_SIZE // 2
+                    ),
+                    CELL_SIZE // 2 - SPACE,
+                    O_WIDTH
+                )
